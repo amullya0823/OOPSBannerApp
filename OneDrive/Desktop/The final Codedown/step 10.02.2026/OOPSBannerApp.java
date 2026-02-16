b257f2451eaf0e21c55363e89b8d8279c4777afe
amullya0823 <amullyasham@gmail.com>
2026-02-16 08:40:43 +0530

    Coded UC1 to print OOPS to console

---
+++ OneDrive/Desktop/The final Codedown/step 10.02.2026/OOPSBannerApp.java
@@ -0,0 +1,6 @@
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // This line tells the computer to shout OOPS in the console
+        System.out.println("OOPS");
+    }
+}
