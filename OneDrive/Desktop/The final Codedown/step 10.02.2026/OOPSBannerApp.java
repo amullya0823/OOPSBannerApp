02ceeb2e8be287a4d65fcd360261679e8dbaf68f
amullya0823 <amullyasham@gmail.com>
2026-02-16 21:14:28 +0530

    Remove comment from OOPSBannerApp
    Removed comment explaining the print statement.

--- OneDrive/Desktop/The final Codedown/step 10.02.2026/OOPSBannerApp.java
+++ OneDrive/Desktop/The final Codedown/step 10.02.2026/OOPSBannerApp.java
@@ -1,6 +1,5 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        // This line tells the computer to shout OOPS in the console
         System.out.println("OOPS");
     }
 }
